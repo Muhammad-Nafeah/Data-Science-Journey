39bff06305750b7a65d62763eb7d545509af5cae
Syed Muhammad Nafeah <syedmuhammadnafeah@gmail.com>
2026-05-16 19:25:15 +0500

    Adding New Practice Problems in Numpy

--- Phase1_Python/Week1_Python_Numpy_Git/Numpy/practice.py
+++ Phase1_Python/Week1_Python_Numpy_Git/Numpy/practice.py
@@ -96,3 +96,64 @@ classroom = np.array([
 bonus_marks = np.array([5, 10, 15])
 
 print(classroom + bonus_marks)
+
+# Create an array of numbers from 1 to 20 with step 3.
+
+array = np.arange(1,20,3) #start -> 1 , stop -> 20 not included , step -> 3
+print(array)
+
+#Create a 4x4 array of zeros and a 2x3 array of ones.
+array = np.zeros((4,4))
+print(array)
+
+array = np.ones((2,3))
+print(array)
+
+# Create 6 evenly spaced numbers between 0 and 100.
+array = np.linspace(0,100,6)  #Start → 0  , Stop → 100 (included) , How many numbers → 6
+print(array)
+
+#Create a 4x5 array of random marks between 40 and 100.
+array = np.random.randint(40,100,(4,5)) #Low → 40 , High → 100 (not included) , Shape → (4, 5)
+print(array)
+
+# Create an array of 6 random marks and sort them in ascending and descending order.
+array = np.random.randint(1,7,6) #Start -> 1 , Stop -> 7 not included , 6 -> 1D array of 6 elements
+sorted_array = np.sort(array)
+print(f"Sorted Array:  {sorted_array}")
+reverse_sorted = np.sort(array)[::-1]
+print(f"Unsorted Array:  {reverse_sorted}")
+
+
+#Create an array of 5 marks and print the index of the highest and lowest mark.
+marks_array = np.random.randint(1,20,5)
+print(f"Array: {marks_array}")
+highest_marks_index = np.argmax(marks_array)
+print(f"Highest Marks Index: {highest_marks_index}")
+lowest_marks_index = np.argmin(marks_array)
+print(f"Lowest Marks Index: {lowest_marks_index}")
+
+
+
+marks = np.array([88, 76, 45, 88, 60, 76, 34, 21, 44, 21, 34])
+print(f"Marks: {marks}")
+unique_marks = np.unique(marks)
+print(f"Unique and Sorted Marks: {unique_marks}")
+
+#Create an array of 6 marks and use np.where to label each as 'Pass' or 'Fail' (passing mark = 50).
+marks = np.random.randint(40,100,6)
+print(marks)
+passing_marks = np.where(marks >= 50 ,'Pass','Fail')
+print(passing_marks)
+
+marks = np.array([110, 76, -5, 90, 60])
+print(np.clip(marks,0,100))   #Any value above 100 → becomes 100 , Any value below 0 → becomes 0
+
+#Create an array, make a copy using .copy(), change the first element of the copy, and prove the original is unchanged.
+
+marks = np.array([88, 76, 45, 90, 60])
+array1 = marks.copy()
+array1[1] = 86
+print("Original Array: ", marks)
+print("New Array: ", array1)
+
